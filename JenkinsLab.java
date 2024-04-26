7133c7356f55e223558664802b30c90e8531a819
SKP-WileyEdge <kpsheetal7@gmail.com>
2024-04-26 12:32:03 +0530

    second commit

--- JenkinsLab.java
+++ JenkinsLab.java
@@ -1,5 +1,5 @@
 public class JenkinsLab{
     public static void main(String[] args){
-        System.out.println(x:"Author:Sheetal - github Jenkins Build Demo");
+        System.out.println("Author:Sheetal - github Jenkins Build Demo");
     }
 }
